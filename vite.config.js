d8fe03ade0e9662977155185215f35399b9498f3
Ishan <ishanpatel0429@gmail.com>
2025-09-04 18:35:51 +0530

    build config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio1/",
+  base: './', // ✅ ensures assets work from root
   plugins: [react()],
 })
